0b58f5f02b045634e18b1a62ed8143ef761bddb1
Martin Milan <martin.milan3@gmail.com>
2022-08-22 16:02:38 -0300

    Commit inicial

---
+++ src/test/java/com/mm/bci/desafio/apiusuarios/ApiusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.bci.desafio.apiusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
